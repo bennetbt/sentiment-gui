b452ad2adbe5707cb1117c0c4713e2a6a74d4171
Evenwood <72255054+Evenwood@users.noreply.github.com>
2023-03-20 12:56:17 -0400

    Prepared GUI based on original html

--- SentimentAnalysis/SentimentAnalysis/Data/SentimentSet.cs
+++ SentimentAnalysis/SentimentAnalysis/Data/SentimentSet.cs
@@ -1,13 +1,11 @@
 namespace SentimentAnalysis.Data
 {
-    public class WeatherForecast
+    public class SentimentSet
     {
-        public DateOnly Date { get; set; }
+        public string Text { get; set; }
 
-        public int TemperatureC { get; set; }
+        public string Sentiment { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
+        public double Score { get; set; }
     }
 }
